dbe7ef3e770fcb05c6e56cc75e78c42efa4c25da
Csabi <alfred06sas@gmail.com>
2013-03-22 13:55:33 +0100

    Initial commit

---
+++ defaultname/audiolovenation/src/program/Field.java
@@ -0,0 +1,81 @@
+package program;
+
+import java.util.*;
+
+public class Field {
+
+	private Collection<Item> items;
+	private Field neighbours;
+	private Smell actualField;
+
+	/**
+	 * 
+	 * @param Item
+	 * @return 
+	 */
+	public void addItem(int Item) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public List<Item> getItems() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Item
+	 * @return 
+	 */
+	public void removeItem(int Item) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public Map<Dir, Field> getNeighbours() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Dir
+	 * @param Field
+	 * @return 
+	 */
+	public void addNeighbour(int Dir, int Field) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public List<Smell> getSmell() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Smell
+	 * @return 
+	 */
+	public void addSmell(int Smell) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Smell
+	 * @return 
+	 */
+	public void removeSmell(int Smell) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ defaultname/audiolovenation/src/program/Food.java
@@ -0,0 +1,24 @@
+package program;
+
+public class Food extends Item {
+
+	private FoodSmell foodSmells;
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public void deleteSmell() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param FoodSmell
+	 * @return 
+	 */
+	public void addFoodSmell(int FoodSmell) {
+		throw new UnsupportedOperationException();
+	}
+
+}
