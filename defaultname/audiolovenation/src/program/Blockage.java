547466dc50ee9441996cb0bb2c82bb88cb07b59c
Csabi <alfred06sas@gmail.com>
2013-03-22 13:55:33 +0100

    Initial commit

---
+++ defaultname/audiolovenation/src/program/Blockage.java
@@ -0,0 +1,4 @@
+package program;
+
+public class Blockage extends Item {
+}
---
+++ defaultname/audiolovenation/src/program/Echidna.java
@@ -0,0 +1,29 @@
+package program;
+
+public class Echidna extends Item implements Movable {
+
+	private Boolean isActive = false;
+	private Integer wait;
+	private Integer hunger;
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public void decreaseHunger() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAlive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
