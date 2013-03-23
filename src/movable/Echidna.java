67ddbe7897ab87ea0b33fa9756f616baf88c41e5
Csabi <Csabi@Csabi>
2013-03-23 03:12:37 +0100

    fdf

--- src/movable/Echidna.java
+++ src/movable/Echidna.java
@@ -1,5 +1,6 @@
 package movable;
 
+import program.Singleton;
 import item.Item;
 
 public class Echidna extends Item implements Movable {
@@ -25,4 +26,27 @@ public class Echidna extends Item implements Movable {
 		
 	}
 
+	/**
+	 * @param ant
+	 *            -
+	 * @param b
+	 *            -
+	 */
+	@Override
+	public void collisionWithAnt(Ant ant, boolean b) {
+		Singleton s = Singleton.Instance();
+
+		Integer id = s.stack.get(s.stack.size() - 1);
+
+		s.makeSpace(">> CALL: " + id + ": Echidna.collisionWithAnt("
+				+ s.ants.indexOf(ant) + ": Ant, " + String.valueOf(b) + ")");
+		s.depth--;
+
+		ant.kill();
+		
+		s.makeSpace("<< RETURN: " + id + ": Echidna.collisionWithAnt("
+				+ s.ants.indexOf(ant) + ": Ant, " + String.valueOf(b) + ")");
+		s.depth--;
+	}
+	
 }
