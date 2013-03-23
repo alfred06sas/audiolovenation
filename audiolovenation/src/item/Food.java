ac721a9386ef9dac4f3568ad03c571926784188c
sylica <sylica@sylica-PC>
2013-03-23 15:59:01 +0100

    Pina

---
+++ audiolovenation/src/item/Food.java
@@ -0,0 +1,26 @@
+package item;
+
+import smell.FoodSmell;
+
+public class Food extends Item {
+
+	private FoodSmell foodSmells;
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public void deleteSmell() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param FoodSmell
+	 * @return 
+	 */
+	public void addFoodSmell(int FoodSmell) {
+		throw new UnsupportedOperationException();
+	}
+
+}
