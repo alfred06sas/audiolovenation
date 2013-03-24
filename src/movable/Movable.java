0a6f46ff80ea20902c3234566f869b4e0d517384
Istvan <Istvan@152.66.211.2>
2013-03-24 02:00:39 +0100

    echidna és a movable végleges kommentezése

--- src/movable/Movable.java
+++ src/movable/Movable.java
@@ -1,16 +1,23 @@
 package movable;
 
+/**
+ * 
+ * A mozgásra képes elemek közös tulajdonságai vannak itt eltárolva. Az ezt
+ * megvalósító elemek képesek mozogni a mezõk között, és érzékelni az
+ * ütközéseket.
+ * 
+ * @author audiolovenation
+ * 
+ */
 public interface Movable {
 
 	/**
-	 * 
-	 * @return 
+	 * Léptetés.
 	 */
 	void step();
 
 	/**
-	 * 
-	 * @return 
+	 * Életre keltés (wait == 0)
 	 */
 	void setAlive();
 
