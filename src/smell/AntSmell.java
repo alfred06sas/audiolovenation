fea67257d582855a5c866ed936333be96e7a0ae7
sylica <sylica@sylica-PC>
2013-03-24 15:03:25 +0100

    AntSmell és FoodSmell removeMyself() megcsinálva

--- src/smell/AntSmell.java
+++ src/smell/AntSmell.java
@@ -1,9 +1,28 @@
 package smell;
 
+import land.Field;
 import program.Singleton;
 import item.Volatile;
 
 public class AntSmell extends Smell implements Volatile {
+
+	@Override
+	public void removeMyself() {
+		Singleton s = Singleton.Instance();
+
+		Integer id = s.stack.get(s.stack.size() - 1);
+		
+		s.makeSpace(">> CALL: " + id + ": AntSmell.removeMyself()");
+
+		s.stack.add(11);
+		Field field = s.fields.get(10);
+		
+		field.removeSmell(s.antSmells.get(id));
+		
+		s.depth--;
+		s.makeSpace("<< RETURN: " + id + ": AntSmell.removeMyself()");
+		s.depth--;
+	}
 
 	@Override
 	public void decrease() {
--- src/smell/FoodSmell.java
+++ src/smell/FoodSmell.java
@@ -1,4 +1,26 @@
 package smell;
 
+import land.Field;
+import program.Singleton;
+
 public class FoodSmell extends Smell {
+	
+	@Override
+	public void removeMyself() {
+		Singleton s = Singleton.Instance();
+
+		Integer id = s.stack.get(s.stack.size() - 1);
+		
+		s.makeSpace(">> CALL: " + id + ": FoodSmell.removeMyself()");
+
+		s.stack.add(14);
+		Field field = s.fields.get(13);
+		
+		field.removeSmell(s.foodSmells.get(id));
+		
+		s.depth--;
+		s.makeSpace("<< RETURN: " + id + ": FoodSmell.removeMyself()");
+		s.depth--;
+	}
+	
 }
