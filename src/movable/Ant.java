2b0ab9c2ba0d2fc43a46be96a99ee5e0549fe72f
sylica <sylica@sylica-PC>
2013-04-19 15:07:03 +0200

    asdsdfgdgerrr

--- src/movable/Ant.java
+++ src/movable/Ant.java
@@ -23,7 +23,6 @@ import smell.Smell;
  * @author audiolovenation
  * 
  */
-// all a faszom mint  a gerely
 public class Ant extends Item implements Movable {
 	/**
 	 * Csáp objektum. Ez végzi a továbbhaladási irányok beállítását.
